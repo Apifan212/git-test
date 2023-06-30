946c04460631ea7504c7544a87a8edf367f79d0e
Apifan <Apifan@123.com>
2023-06-30 16:53:50 +0800

    hot-fix commit -v1

--- src/main/java/git/git_test.java
+++ src/main/java/git/git_test.java
@@ -5,5 +5,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println(1111);
+
+        System.out.println("change" + "   by hot-fix");
     }
 }
